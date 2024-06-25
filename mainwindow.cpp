23b81d7c840be21563ba66e70a8cb17087fd888c
Абдурахман Усманов <ausmanov706@gmail.com>
2024-06-26 02:26:19 +0300

    feat: search, sorting, settings

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,40 +1,124 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-// for debug
-#include <QDebug>
-#include <QMessageBox>
-
-// for sql
-#include <QSqlDatabase>
-#include <QSqlQuery>
-#include <QSqlError>
-#include <QSqlResult>
-
-// basic
-#include <QString>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
+    , db(QSqlDatabase::addDatabase("QPSQL"))
+    , connectionName("myConnection")
+    , dModel(nullptr)
+    , proxyModel(nullptr)
 {
     ui->setupUi(this);
 
-    if (QSqlDatabase::isDriverAvailable("QPSQL")) {
+    proxyModel = new QSortFilterProxyModel(ui->tableView);
 
-        qDebug() << "SUCCESS: PostgreSQL driver was found!";
-    }
-    else {
+    /*
+     *  Delegates
+    */
 
-        qDebug() << "FAILED: PostgreSQL driver was not found!";
-    }
+    ui->tableView->setAttribute(Qt::WA_DeleteOnClose);
+    ui->tableView->setContextMenuPolicy(Qt::CustomContextMenu);
+
+    /*
+     * Set Delegates -> (-_-)
+     */
+
+    ui->tableView->setSelectionMode(QAbstractItemView::ExtendedSelection);
+    ui->tableView->dragDropOverwriteMode();
+    ui->tableView->setDragEnabled(true);
+    ui->tableView->setAcceptDrops(true);
+    ui->tableView->setDropIndicatorShown(true);
+    ui->tableView->setDefaultDropAction(Qt::MoveAction);
+
+    ui->tableView->setSortingEnabled(true);
+
+    qApp->installTranslator(&appTranslator);
+    qApp->installTranslator(&qtTranslator);
+
+    qmPath =  + "../cw-sport_2/translations";
+
+    createLanguageMenu();
+
+    setSettings();
+
 }
 
 MainWindow::~MainWindow()
 {
+    getSettings();
     delete ui;
 }
 
+void MainWindow::getSettings() {
+    QSettings settings("Settings", "MySettings");
+
+    settings.beginGroup("MainWindowGeometry");
+    settings.setValue("size", size());
+    settings.setValue("position", pos());
+    settings.setValue("language", languageActionGroup->checkedAction()->data().toString());
+    settings.endGroup();
+}
+
+void MainWindow::setSettings() {
+    QSettings settings("Settings", "MySettings");
+
+    settings.beginGroup("MainWindowGeometry");
+    resize(settings.value("size").toSize());
+    move(settings.value("position").toPoint());
+
+    QString savedLanguage = settings.value("language").toString();
+
+    foreach(QAction *action, languageActionGroup->actions()) {
+        if (action->data().toString() == savedLanguage) {
+            action->setChecked(true);
+            switchLanguage(action);
+            break;
+        }
+    }
+
+    settings.endGroup();
+}
+
+void MainWindow::on_tableView_customContextMenuRequested(const QPoint &pos)
+{
+    QMenu *menu = new QMenu(ui->tableView);
+
+    QModelIndex index=ui->tableView->indexAt(pos);
+    QModelIndex sourceIndex = proxyModel->mapToSource(index);
+
+    QAction *deleteAction = new QAction(tr("Delete row"), ui->tableView);
+    QAction *addAction = new QAction(tr("Add row"), ui->tableView);
+
+    menu->addAction(deleteAction);
+    menu->addAction(addAction);
+
+    QAction *selectedItem = menu->exec(ui->tableView->viewport()->mapToGlobal(pos));
+
+    if (selectedItem == deleteAction && sourceIndex.isValid()) {
+        QMessageBox::StandardButton resBtn =
+            QMessageBox::question(this, tr("Delete row"), tr("Are you sure you want to delete the line?\n"),
+                                  QMessageBox::No | QMessageBox::Yes);
+
+        if (resBtn == QMessageBox::Yes)
+
+            dModel->removeRow(sourceIndex.row());
+        else
+            return;
+
+    } else if (selectedItem == addAction) {
+
+        SportComplex newSportComplex;
+
+        auto placeholder = this;
+
+        if (placeholder == nullptr)
+            return;
+
+        dModel->insertRow(dModel->rowCount(), newSportComplex);
+    }
+}
 
 void MainWindow::on_actionConnect_triggered()
 {
@@ -48,20 +132,168 @@ void MainWindow::on_actionConnect_triggered()
 
 void MainWindow::handleConnectionDetails(const QString &host, const int &port, const QString &database, const QString &user, const QString &password)
 {
-    QSqlDatabase db = QSqlDatabase::addDatabase("QPSQL");
     db.setHostName(host);
+    db.setConnectOptions("requiressl=0;connect_timeout=2");
     db.setPort(port);
     db.setDatabaseName(database);
     db.setUserName(user);
     db.setPassword(password);
+    db.open();
+    /// Проверить открыто ли соединение с БД можно через метод "isOpen()"
+    if (!db.isOpen()) {
+        /// Сообщение об ошибке можно вывести через диалоговое окно
+        QMessageBox::critical(this,
+                              tr("Connection failure!"),
+                              db.lastError().text());
+        /// и/или в консоль
+        qDebug() << db.lastError().driverText();
+        qDebug() << db.lastError().databaseText();
 
-    if (!db.open()) {
-        QMessageBox::critical(this, "Connection Failed", db.lastError().text());
+        return;
+    }
+
+    if (dModel == nullptr) {
+        /// Используем созданную модель
+        dModel = new DataModel(this, db);
+
+        dModel->setTableName("complexes");
+        /// Инициализируем данные таблицы
+        auto error = dModel->initLoad();
+
+        if (error.type() == QSqlError::NoError) {
+            proxyModel->setSourceModel(dModel);
+            ui->tableView->setModel(proxyModel);
+
+            ui->tableView->resizeColumnsToContents();
+
+            qDebug() << dModel->count();
+        }
+        else
+            QMessageBox::critical(this, tr("Error select!"), error.text());
+    }
+
+    ui->menuTables->setDisabled(false);
+    ui->tableView->verticalHeader()->setVisible(false);
+
+}
+
+
+
+
+void MainWindow::on_actionComplex_Types_triggered()
+{
+    AdditionalTable *typesTable = new AdditionalTable(this, db, "complex_types");
+
+    typesTable->show();
+}
+
+
+void MainWindow::on_actionComplex_Financing_triggered()
+{
+    AdditionalTable *financingTable = new AdditionalTable(this, db, "complex_financing");
+
+    financingTable->show();
+}
+
+
+void MainWindow::on_lineEdit_textChanged(const QString &text)
+{
+    if (!text.isEmpty()) {
+        QModelIndex index = this->tableFind(text);
+        ui->tableView->setCurrentIndex(index);
     } else {
-        QMessageBox::information(this, "Connection Successful", "Connected to the database successfully!");
+        this->resetFind();
     }
 
 }
 
+void MainWindow::createLanguageMenu()
+{
+    /// Создаём список пунктов выбора языка в динамической памяти
+    languageActionGroup = new QActionGroup(this);
+
+    /// Ставим связь пунктов меню со слотом смены языка приложения
+    connect(languageActionGroup, &QActionGroup::triggered, this, &MainWindow::switchLanguage);
+
+    /// Определяем каталог, где лежат файлы переводов "*.qm"
+    QDir dir(qmPath);
+
+    QStringList fileNames =
+        dir.entryList(QStringList("cw-sport-2_*.qm"));
+
+    for (int i = 0; i < fileNames.size(); i++) {
+        /// Получаем i-ую локаль
+        QString locale = fileNames[i];
+        /// Удаляем символы в строке сначала и по символ "_" включительно
+        locale.remove(0, locale.indexOf('_') + 1);
+        // Удаляем символы в строке начиная с символа точки
+        locale.truncate(locale.lastIndexOf('.'));
+
+        /// Создаём временную переменную перевода для языкового пункта меню
+        QTranslator translator;
+        /// Загружаем перевод из файла перевода "*.qm"
+        translator.load(fileNames[i], qmPath);
+
+        QString language = translator.translate("MainWindow", "English");
+        QAction *action = new QAction(tr("&%1 %2").arg(i + 1).arg(language));
+
+        action->setCheckable(true);
+        /// Задаём внутренние данные хранимые в i-ом пункте.
+        /// В нашем случае это локаль вида "ru_RU"
+        action->setData(locale);
+
+        /// Добавляем i-ый пункт в меню на нашей форме "mainwindow.ui"
+        ui->menuLanguage->addAction(action);
+        /// Добавляем i-ый пункт в единую группу пунктов
+        languageActionGroup->addAction(action);
+
+        ///  Задаём  английский язык в качестве выбранного по умолчанию
+        if (language == "English")
+            action->setChecked(true);
+    }
+
+}
+
+void MainWindow::switchLanguage(QAction *action)
+{
+    QString locale = action->data().toString();
+
+    /// Загружаем тот перевод который выбрал пользователь
+    appTranslator.load("cw-sport-2_" + locale, qmPath);
+    locale.chop(3);
+
+    qtTranslator.load("qt_" + locale + ".qm", QLibraryInfo::location(QLibraryInfo::TranslationsPath));
+
+    ui->retranslateUi(this);
+}
+
+
+QModelIndex MainWindow::tableFind(const QString &text)
+{
+    QModelIndex index = ui->tableView->currentIndex();
+
+    /// Если в таблице нет выделенных ячеек, устанавливаем фильтр на указанный столбец
+    if (!index.isValid()) {
+        proxyModel->setFilterKeyColumn(selectedColumn);
+    } else {
+        selectedColumn = index.column();
+        proxyModel->setFilterKeyColumn(selectedColumn);
+    }
+
+    proxyModel->setFilterFixedString(text);
+
+    return index;
+}
+
+void MainWindow::resetFind()
+{
+    proxyModel->setFilterFixedString(QString());
+
+    /// Если в таблице нет выделенных ячеек, сбрасываем фильтр на все столбцы
+    if (!ui->tableView->currentIndex().isValid()) {
+        selectedColumn = -1;
+        proxyModel->setFilterKeyColumn(selectedColumn);
+    }
+}
 
 
--- mainwindow.h
+++ mainwindow.h
@@ -4,8 +4,27 @@
 #include "connectwindow.h"
 
 #include <QMainWindow>
-
+#include <QMainWindow>
+#include <QSortFilterProxyModel>
+#include <QMessageBox>
+#include <QSettings>
 #include <QString>
+#include <QMenu>
+#include <QDebug>
+#include <QTranslator>
+#include <QLibraryInfo>
+#include <QDir>
+#include <QDropEvent>
+
+#include <QSqlDatabase>
+#include <QSqlError>
+#include <QSqlRecord>
+#include <QSqlQuery>
+#include <QSqlTableModel>
+
+#include <datamodel.h>
+#include <additionaltable.h>
+#include <connectwindow.h>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -19,11 +38,48 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+    /// Метод получения настрое приложения
+    void getSettings();
+
+    /// Метод установки настроек приложения
+    void setSettings();
+
+    /// Создаёт меню для выбора языка
+    void createLanguageMenu();
+
+    /// Меняет текущий язык на выбранный в меню
+    void switchLanguage(QAction *action);
+
+    QModelIndex tableFind(const QString&);
+
+    void resetFind();
+
 private slots:
     void on_actionConnect_triggered();
+
     void handleConnectionDetails(const QString &host, const int &port, const QString &database, const QString &user, const QString &password);
+
+    void on_tableView_customContextMenuRequested(const QPoint &pos);
+
+    void on_lineEdit_textChanged(const QString &arg1);
+
+    void on_actionComplex_Types_triggered();
+
+    void on_actionComplex_Financing_triggered();
 
 private:
     Ui::MainWindow *ui;
+
+    QString connectionName;
+    QSqlDatabase db;
+    int selectedColumn;
+
+    DataModel *dModel;
+    QSortFilterProxyModel *proxyModel;
+
+    QTranslator appTranslator;
+    QTranslator qtTranslator;
+    QActionGroup *languageActionGroup;
+    QString qmPath;
 };
 #endif // MAINWINDOW_H
